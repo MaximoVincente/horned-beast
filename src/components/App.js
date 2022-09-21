dc6a514489d2397303f0ffadadb6f9f0fd1d88c9
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-09-21 15:32:12 -0400

    added main

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+         <Main />
+        <Footer />
+      </>
+    );
+  }
+}
+
+export default App;
